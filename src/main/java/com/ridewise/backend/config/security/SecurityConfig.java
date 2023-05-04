d959597efcad556e3d12a3cc6155e4c96b2f4dd2
paweu9000 <wolfenorrin5@gmail.com>
2023-05-04 18:11:10 +0200

    [NEW] initial SecurityConfig

---
+++ src/main/java/com/ridewise/backend/config/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.ridewise.backend.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+                configuration.setAllowedMethods(Arrays.asList("GET", "DELETE", "POST", "PUT"));
+                configuration.setMaxAge(3600L);
+                configuration.setAllowCredentials(true);
+                configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type",
+                        "x-auth-token", "Authorization"));
+                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+                return configuration;
+            }
+        }).and().csrf().disable().authorizeHttpRequests()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+}
