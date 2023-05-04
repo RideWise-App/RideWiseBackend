d503f6d7f4983b342cfdd9bed882cf781134bc65
paweu9000 <wolfenorrin5@gmail.com>
2023-05-04 18:57:22 +0200

    [NEW] repository + service basic operations implementation

--- src/main/java/com/ridewise/backend/service/ClientService.java
+++ src/main/java/com/ridewise/backend/service/ClientService.java
@@ -1,12 +1,27 @@
 package com.ridewise.backend.service;
 
+import com.ridewise.backend.entity.Client;
 import com.ridewise.backend.repository.ClientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class ClientService {
 
     final ClientRepository repository;
+
+    Client findById(Long id) {
+        return clientCheck(repository.findById(id));
+    }
+
+    Client findByEmail(String email) {
+        return clientCheck(repository.findByEmail(email));
+    }
+
+    Client clientCheck(Optional<Client> optionalClient) {
+        return optionalClient.orElseThrow(() -> new RuntimeException("not found"));
+    }
 }
