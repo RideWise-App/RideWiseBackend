5f5c2786367225f89a8970e6cce4fd251b529bda
paweu9000 <wolfenorrin5@gmail.com>
2023-05-04 18:43:28 +0200

    [NEW] passwordEncoder bean

--- src/main/java/com/ridewise/backend/security/SecurityConfig.java
+++ src/main/java/com/ridewise/backend/security/SecurityConfig.java
@@ -1,8 +1,9 @@
-package com.ridewise.backend.config.security;
+package com.ridewise.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -32,5 +33,10 @@ class SecurityConfig {
         }).and().csrf().disable().authorizeHttpRequests()
                 .anyRequest().authenticated();
         return http.build();
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
