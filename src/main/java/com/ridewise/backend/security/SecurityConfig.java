68cb9d0725197de6f1d562232221482831e945f3
paweu9000 <wolfenorrin5@gmail.com>
2023-05-05 12:44:54 +0200

    [MAJOR] initial JWTAuthorizationFilter + implementation

--- src/main/java/com/ridewise/backend/security/SecurityConfig.java
+++ src/main/java/com/ridewise/backend/security/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.ridewise.backend.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,13 +17,19 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
+@AllArgsConstructor
 class SecurityConfig {
 
     @Value("${jwt-key}")
     static String secretKey;
 
+    AuthenticationManager authenticationManager;
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager);
+        authenticationFilter.setFilterProcessesUrl("/api/login");
+
         http.cors().configurationSource(new CorsConfigurationSource() {
             @Override
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
@@ -35,12 +44,17 @@ class SecurityConfig {
                 return configuration;
             }
         }).and().csrf().disable().authorizeHttpRequests()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(authenticationFilter)
+                .addFilterAfter(new JWTAuthorizationFilter(), AuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
         return http.build();
     }
 
     @Bean
-    BCryptPasswordEncoder passwordEncoder() {
+    static BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
