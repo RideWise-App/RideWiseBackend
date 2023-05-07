3ceb5cf63c0744dfc31ba7d79c90fc42304ba83d
paweu9000 <wolfenorrin5@gmail.com>
2023-05-07 18:39:18 +0200

    [MAJOR] DTO class to record fix

--- src/main/java/com/ridewise/backend/dto/ClientLoginDto.java
+++ src/main/java/com/ridewise/backend/dto/ClientLoginDto.java
@@ -1,7 +1,5 @@
 package com.ridewise.backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
@@ -9,6 +7,4 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-@AllArgsConstructor
-@Getter
 public record ClientLoginDto(@Email String email, @NotBlank @NotEmpty @NotNull @Length(min = 8) String password) {}
--- src/main/java/com/ridewise/backend/dto/ClientRegisterDto.java
+++ src/main/java/com/ridewise/backend/dto/ClientRegisterDto.java
@@ -1,7 +1,5 @@
 package com.ridewise.backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
@@ -9,8 +7,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-@AllArgsConstructor
-@Getter
 public record ClientRegisterDto(@NotBlank @NotEmpty @NotNull @Length(max = 50) String firstName,
                                 @NotBlank @NotEmpty @NotNull @Length(max = 50) String lastName, @Email String email,
                                 @NotBlank @NotEmpty @NotNull @Length(min = 8) String password) {}
