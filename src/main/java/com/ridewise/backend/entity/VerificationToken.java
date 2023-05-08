e9ee03b47ccc5e2ac1a6bbb02715de3fd7aa6b22
paweu9000 <wolfenorrin5@gmail.com>
2023-05-08 16:24:23 +0200

    [MAJOR] initial verificationToken entity + repo and service

---
+++ src/main/java/com/ridewise/backend/entity/VerificationToken.java
@@ -0,0 +1,26 @@
+package com.ridewise.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerificationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_client")
+    private Client client;
+
+}
