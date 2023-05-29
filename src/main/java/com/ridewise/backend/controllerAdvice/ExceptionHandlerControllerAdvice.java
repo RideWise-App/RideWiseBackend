6aa6fd8998f666e5ee3528145807a306c5b19c35
paweu9000 <wolfenorrin5@gmail.com>
2023-05-29 18:06:34 +0200

    [MINOR] ExceptionHandler fix

--- src/main/java/com/ridewise/backend/controllerAdvice/ExceptionHandlerControllerAdvice.java
+++ src/main/java/com/ridewise/backend/controllerAdvice/ExceptionHandlerControllerAdvice.java
@@ -2,39 +2,41 @@ package com.ridewise.backend.controllerAdvice;
 
 import com.ridewise.backend.exception.InternalServerErrorException;
 import com.ridewise.backend.exception.RecordNotFoundException;
-import com.ridewise.backend.model.ErrorListResponse;
 import com.ridewise.backend.model.ErrorResponse;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
-public class ExceptionHandlerControllerAdvice {
+class ExceptionHandlerControllerAdvice extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({RecordNotFoundException.class})
+    @ExceptionHandler({RuntimeException.class})
     public ResponseEntity<Object> handleRecordNotFoundException(Exception ex) {
-        String message = ex.getMessage();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(message));
+        ExceptionMessageDto exceptionMessage = new ExceptionMessageDto(List.of(ex.getMessage()));
+        return new ResponseEntity<>(exceptionMessage, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({InternalServerErrorException.class})
     public ResponseEntity<Object> handleInternalServerErrorException(Exception ex) {
-        String message = ex.getMessage();
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(message));
+        ExceptionMessageDto exceptionMessage = new ExceptionMessageDto(List.of(ex.getMessage()));
+        return new ResponseEntity<>(exceptionMessage, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(value = {DataIntegrityViolationException.class})
     public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
         List<String> errors = new ArrayList<>();
 
-        if (ex.getCause() instanceof ConstraintViolationException) {
-            ConstraintViolationException constraintViolationEx = (ConstraintViolationException) ex.getCause();
+        if (ex.getCause() instanceof ConstraintViolationException constraintViolationEx) {
             if (constraintViolationEx.getConstraintName().endsWith("_unq")) {
                 errors.add("Unique constraint violation: " + ex.getMessage());
             } else if (constraintViolationEx.getConstraintName().contains("_fk_")) {
@@ -49,7 +51,16 @@ public class ExceptionHandlerControllerAdvice {
         if (errors.isEmpty()) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("An unexpected error occurred"));
         } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorListResponse(errors));
+            return new ResponseEntity<>(new ExceptionMessageDto(errors), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().forEach(objectError -> errors.add(objectError.getDefaultMessage()));
+        return new ResponseEntity<>(new ExceptionMessageDto(errors), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/ridewise/backend/security/ExceptionHandlerFilter.java
+++ src/main/java/com/ridewise/backend/security/ExceptionHandlerFilter.java
@@ -14,7 +14,7 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
                                     FilterChain filterChain) throws ServletException, IOException {
         try {
             filterChain.doFilter(request, response);
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
             response.getWriter().write(e.getMessage());
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             response.getWriter().flush();
