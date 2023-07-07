5d88549ce558e88b6cb9562d959b308378945040
paweu9000 <wolfenorrin5@gmail.com>
2023-07-07 10:26:19 +0200

    [MINOR] intial orderService and locationDto + ordercontroller

---
+++ src/main/java/com/ridewise/backend/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.ridewise.backend.controller;
+
+import com.ridewise.backend.dto.LocationDto;
+import com.ridewise.backend.serviceImpl.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/order")
+record OrderController(OrderService service) {
+
+    @PreAuthorize("hasRole('USER')")
+    @PostMapping("")
+    ResponseEntity<HttpStatus> createOrder(@RequestBody Map<String, LocationDto> locations) {
+        service.initializeOrder(locations);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
