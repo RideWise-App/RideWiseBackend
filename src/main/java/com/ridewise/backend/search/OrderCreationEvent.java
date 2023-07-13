dc2f41a4c5a540cc4670bf18781ba73ee5376117
paweu9000 <wolfenorrin5@gmail.com>
2023-07-13 18:51:06 +0200

    [MAJOR] order refactor to contain orderDto

--- src/main/java/com/ridewise/backend/search/OrderCreationEvent.java
+++ src/main/java/com/ridewise/backend/search/OrderCreationEvent.java
@@ -1,17 +1,18 @@
 package com.ridewise.backend.search;
 
+import com.ridewise.backend.dto.OrderDto;
 import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
 @Getter
 public class OrderCreationEvent extends ApplicationEvent {
-    final Long orderId;
+    final OrderDto order;
     final double latitude;
     final double longitude;
 
-    public OrderCreationEvent(Object source, Long orderId, double latitude, double longitude) {
+    public OrderCreationEvent(Object source, OrderDto order, double latitude, double longitude) {
         super(source);
-        this.orderId = orderId;
+        this.order = order;
         this.latitude = latitude;
         this.longitude = longitude;
     }
