df61c7a5f3b2e08de66ff18ffc14026c93c5c599
paweu9000 <wolfenorrin5@gmail.com>
2023-07-14 16:54:16 +0200

    [MAJOR] setup for accepting order

--- src/main/java/com/ridewise/backend/serviceImpl/OrderService.java
+++ src/main/java/com/ridewise/backend/serviceImpl/OrderService.java
@@ -2,6 +2,7 @@ package com.ridewise.backend.serviceImpl;
 
 import com.ridewise.backend.dto.LocationDto;
 import com.ridewise.backend.dto.OrderDto;
+import com.ridewise.backend.entity.Driver;
 import com.ridewise.backend.entity.Location;
 import com.ridewise.backend.entity.Order;
 import com.ridewise.backend.mapper.LocationMapper;
@@ -20,13 +21,15 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final ClientService clientService;
     private final LocationService locationService;
+    private final DriverService driverService;
     private final ApplicationEventPublisher publisher;
 
     OrderService(OrderRepository theOrderRepository, ClientService clientService,
-                 LocationService locationService, ApplicationEventPublisher publisher) {
+                 LocationService locationService, DriverService driverService, ApplicationEventPublisher publisher) {
         orderRepository = theOrderRepository;
         this.clientService = clientService;
         this.locationService = locationService;
+        this.driverService = driverService;
         this.publisher = publisher;
     }
 
@@ -62,5 +65,13 @@ public class OrderService {
 
     List<OrderDto> availableOrders(List<OrderDto> orders) {
         return orders.stream().filter(order -> order.driverId() == null).toList();
+    }
+
+    public void acceptOrder(Long id, String driver) {
+        Driver entity = driverService.findByEmail(driver);
+        Order order = orderRepository.findById(id).orElseThrow();
+        if (order.getDriverId() == null) {
+            order.setDriverId(entity.getId());
+        } else throw new RuntimeException("This order is taken");
     }
 }
